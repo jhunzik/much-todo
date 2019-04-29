778102168ab85065bfe6c3c7497122d24762ad1d
jhunzik <joshah54@gmail.com>
2019-04-28 21:28:29 -0700

    Can add tasks to the list on the page

--- vanilla/src/index.js
+++ vanilla/src/index.js
@@ -1,1 +1,16 @@
 import "./index.css";
+
+var items = [];
+var addButton = document.getElementById("add-button");
+
+function add() {
+    var item = document.getElementById("input-field");
+    var itemValue = item.value;
+    var list = document.getElementById("td-list");
+    var listItem = document.createElement("LI");
+
+    items.push(itemValue);
+    listItem.appendChild(document.createTextNode(itemValue));
+    list.appendChild(listItem);
+}
+addButton.addEventListener("click", add);
